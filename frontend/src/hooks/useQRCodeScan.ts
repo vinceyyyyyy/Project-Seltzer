c9ae6ca4baca093bb604b5216997761373c10dcb
Vincent Yan <vincent.yan@blend360.com>
2022-10-08 22:05:57 -0400

    feat: implement QR code scanner

---
+++ frontend/src/hooks/useQRCodeScan.ts
@@ -0,0 +1,139 @@
+import { useState, useEffect, useRef } from "react";
+import { Html5Qrcode } from "html5-qrcode";
+
+export default function useQRCodeScan({
+  qrcodeMountNodeID = "",
+  closeAfterScan = true,
+  qrCodeScannerDimension,
+}: {
+  qrcodeMountNodeID?: string;
+  closeAfterScan?: boolean;
+  qrCodeScannerDimension: { width: number; height: number };
+}) {
+  const [decodedQRData, setDecodedQrData] = useState<{
+    isScanning: boolean;
+    isScanSuccess: boolean;
+    isScanFailure: boolean;
+    data: string | null;
+    error: any | null;
+  }>({
+    isScanning: false,
+    isScanSuccess: false,
+    isScanFailure: false,
+    data: null,
+    error: null,
+  });
+  const html5QrCodeScannerRef = useRef<Html5Qrcode | null>(null);
+
+  // unmount logic
+  useEffect(() => {
+    return () => {
+      if (html5QrCodeScannerRef.current) {
+        html5QrCodeScannerRef.current
+          ?.stop()
+          ?.then((ignore) => {
+            // QR Code scanning is stopped
+            console.log("stopped after successful scan");
+          })
+          ?.catch((err) => {
+            // Stop failed, handle it.
+            console.log("fails to stop after successfull scan result ");
+          });
+      }
+    };
+  }, []);
+
+  function stopQrCode() {
+    if (html5QrCodeScannerRef.current) {
+      html5QrCodeScannerRef.current
+        ?.stop()
+        ?.then((ignore) => {
+          // QR Code scanning is stopped
+          console.log("stopped after successful scan");
+        })
+        ?.catch((err) => {
+          // Stop failed, handle it.
+          console.log("fails to stop after successfull scan result ");
+        });
+    }
+  }
+
+  function startQrCode() {
+    try {
+      setDecodedQrData({
+        ...decodedQRData,
+        isScanning: true,
+        data: null,
+      });
+      // eslint-disable-next-line
+      const html5qrCodeScanner = new Html5Qrcode(qrcodeMountNodeID, {
+        experimentalFeatures: {
+          useBarCodeDetectorIfSupported: false,
+        },
+        verbose: true,
+      });
+
+      html5QrCodeScannerRef.current = html5qrCodeScanner;
+
+      html5qrCodeScanner
+        .start(
+          // { deviceId: { exact: cameraId } },
+          { facingMode: "environment" },
+
+          { fps: 10, qrbox: qrCodeScannerDimension },
+          (qrCodeMessage) => {
+            // do something when code is read
+            window.alert(`scanned qr code, ${qrCodeMessage}`);
+
+            setDecodedQrData({
+              ...decodedQRData,
+              isScanSuccess: true,
+              isScanning: false,
+              data: qrCodeMessage,
+              error: "",
+            });
+
+            if (closeAfterScan) {
+              html5qrCodeScanner
+                .stop()
+                .then((ignore) => {
+                  // QR Code scanning is stopped.
+                  // setIsOpenCamera(false);
+                  console.log("stopped after successful scan");
+                })
+                .catch((err) => {
+                  // Stop failed, handle it.
+                  console.log("fails to stop after succesfull scan result ");
+                });
+            }
+          },
+          (errorMessage) => {}
+        )
+        .catch((err) => {
+          setDecodedQrData({
+            ...decodedQRData,
+            isScanSuccess: false,
+            isScanning: false,
+            isScanFailure: true,
+            data: null,
+            error: err || "QR Code parsing failed",
+          });
+        });
+    } catch (e) {
+      setDecodedQrData({
+        ...decodedQRData,
+        isScanSuccess: false,
+        isScanning: false,
+        isScanFailure: true,
+        data: null,
+        error: e || "QR Code parsing failed",
+      });
+    }
+  }
+
+  return {
+    startQrCode,
+    decodedQRData,
+    stopQrCode,
+  };
+}
--- frontend/src/pages/scan/index.tsx
+++ frontend/src/pages/scan/index.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+import useQRCodeScan from "../../hooks/useQRCodeScan";
 
 export default function Scan() {
+  const { startQrCode, decodedQRData } = useQRCodeScan({
+    qrcodeMountNodeID: "qrcode-scanner",
+    qrCodeScannerDimension: { width: 400, height: 400 },
+  });
+
+  useEffect(() => startQrCode, []);
+
   return (
     <div className="container mx-auto">
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <div className="flex flex-col items-center justify-center w-full space-y-4">
           <h1 className="text-4xl font-bold">Scan</h1>
-          <p className="text-gray-500">This is the scan page.</p>
+          <div id="qrcode-scanner" className={"w-120"}></div>
         </div>
       </div>
     </div>
