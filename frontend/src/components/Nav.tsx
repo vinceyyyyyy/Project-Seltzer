35a69b9cbd6063d86cc7d3ce47a4f52d55a9802d
Vincent Yan <vincent.yan@blend360.com>
2022-10-12 14:28:11 -0400

    test

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,5 +1,5 @@
-import React, { useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import React, {useRef, useState} from "react";
+import {Link} from "react-router-dom";
 import useOutsideClick from "../hooks/useOutsideClick";
 
 export default function Nav(props: {
@@ -16,7 +16,7 @@ export default function Nav(props: {
     <li key={item.name}>
       <Link
         to={item.path}
-        className="block py-2 pr-4 pl-3 text-white rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+        className="block py-2 pr-4 pl-3 text-white rounded md:bg-transparent md:text-blue-700 md:p-0"
       >
         {item.name}
       </Link>
@@ -24,15 +24,15 @@ export default function Nav(props: {
   ));
 
   return (
-    <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+    <nav className="bg-gray-900 border-gray-200 px-2 sm:px-4 py-2.5 rounded">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <div className="self-center text-xl font-semibold whitespace-nowrap dark:text-white p-2">
+        <div className="self-center text-xl font-semibold whitespace-nowrap text-white p-2">
           Project Seltzer
         </div>
         <div ref={wrapperRef}>
           <button
             type="button"
-            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 ml-3 text-sm rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
             onClick={() => {
               setIsNavExpanded(!isNavExpanded);
             }}
@@ -58,7 +58,8 @@ export default function Nav(props: {
           }w-full md:block md:w-auto`}
           id="navbar-default"
         >
-          <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul
+            className="flex flex-col p-4 mt-4 rounded-lg border md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white bg-gray-800 md:bg-gray-900 border-gray-700">
             {menuItemList}
           </ul>
         </div>
