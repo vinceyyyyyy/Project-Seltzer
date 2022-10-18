2e8b1f8d6e04672959f71019055dbaf433883278
Vincent Yan <vincent.yan@blend360.com>
2022-10-18 14:02:57 -0400

    chore: remove unnecessary config file and test
    The project is too simple for unit test

--- backend/tests/unit/getInventory.test.ts
+++
@@ -1,8 +0,0 @@
-import { lambdaHandler } from "../../src/handlers/getInventory";
-
-describe("Unit test for getItemsFromDB handler", function () {
-  it("verifies successful response", async () => {
-    const result = await lambdaHandler();
-    expect(result.statusCode).toEqual(200);
-  });
-});
