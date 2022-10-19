90ad7a51c806d86ba27e0025de22522d35a80bf6
Vincent Yan <vincent.yan@blend360.com>
2022-10-19 13:59:58 -0400

    refactor: replace wishlist with history

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from "react";
 import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
 import Root from "./routes/Root";
 import Inventory from "./pages/inventory";
 import Scan from "./pages/scan";
-import Wishlist from "./pages/wishlist";
+import History from "./pages/history";
 
 const menuItems = [
   { name: "Inventory", path: "/inventory" },
   { name: "Scan", path: "/scan" },
-  { name: "Wishlist", path: "/wishlist" },
+  { name: "History", path: "/history" },
 ];
 
 const router = createBrowserRouter([
@@ -17,16 +16,15 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root menuItems={menuItems} />,
     children: [
+      { path: "/", element: <Inventory /> },
       { path: "inventory", element: <Inventory /> },
       { path: "scan", element: <Scan /> },
-      { path: "wishlist", element: <Wishlist /> },
+      { path: "history", element: <History /> },
     ],
   },
 ]);
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className={"w-full"}>
       <RouterProvider router={router} />
--- frontend/src/pages/history/index.tsx
+++ frontend/src/pages/history/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function Wishlist() {
+export default function History() {
   return (
     <div className="container mx-auto">
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <div className="flex flex-col items-center justify-center w-full space-y-4">
-          <h1 className="text-4xl font-bold">Wishlist</h1>
-          <p className="text-gray-500">This is the wishlist page.</p>
+          <h1 className="text-4xl font-bold">History</h1>
+          <p className="text-gray-500">This is the history page.</p>
         </div>
       </div>
     </div>
